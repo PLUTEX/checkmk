1bdff620b2727ee43c556a09d99b4e2bdce4774a
Jan-Philipp Litza <jpl@plutex.de>
2022-07-26 10:36:13 +0200

    snmp(classic): Preserve newlines

--- cmk/fetchers/snmp_backend/_utils.py
+++ cmk/fetchers/snmp_backend/_utils.py
@@ -36,7 +36,7 @@ def _is_hex_string(value: str) -> bool:
     n = 0
     for x in value:
         if n % 3 == 2:
-            if x != ' ':
+            if x != ' ' and x != '\n':
                 return False
         else:
             if x not in hexdigits:
