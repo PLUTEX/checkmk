e3f598f52b120e1fcaeefcf651a2115adf8fae3e
Jan-Philipp Litza <jpl@plutex.de>
2022-07-26 10:59:14 +0200

    ucd_cpu_load: Support number of CPUs

--- cmk/base/plugins/agent_based/ucd_cpu_load.py
+++ cmk/base/plugins/agent_based/ucd_cpu_load.py
@@ -4,16 +4,16 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from typing import Optional
+from typing import List, Optional
 
-from .agent_based_api.v1 import register, SNMPTree
+from .agent_based_api.v1 import register, SNMPTree, OIDEnd
 from .agent_based_api.v1.type_defs import StringTable
 from .utils.cpu import Load, Section
 from .utils.ucd_hr_detection import UCD
 
 
-def parse_ucd_cpu_load(string_table: StringTable) -> Optional[Section]:
-    if len(string_table) != 3:
+def parse_ucd_cpu_load(string_table: List[StringTable]) -> Optional[Section]:
+    if len(string_table[0]) != 3:
         return None
     return Section(
         load=Load(
@@ -23,10 +23,10 @@ def parse_ucd_cpu_load(string_table: StringTable) -> Optional[Section]:
                 else float(int_cpu_load_str) / 100.0
                 if int_cpu_load_str
                 else 0
-                for int_cpu_load_str, float_cpu_load_str in string_table
+                for int_cpu_load_str, float_cpu_load_str in string_table[0]
             )
         ),
-        num_cpus=1,
+        num_cpus=max(len(string_table[1]), 1),
     )
 
 
@@ -34,12 +34,18 @@ register.snmp_section(
     name="ucd_cpu_load",
     parsed_section_name="cpu",
     parse_function=parse_ucd_cpu_load,
-    fetch=SNMPTree(
-        base=".1.3.6.1.4.1.2021.10.1",
-        oids=[
-            "5",  # UCD-SNMP-MIB::laLoadInt       Int table
-            "6",  # UCD-SNMP-MIB::laLoadFloat     Float table
-        ],
-    ),
+    fetch=[
+        SNMPTree(
+            base=".1.3.6.1.4.1.2021.10.1",
+            oids=[
+                "5",  # UCD-SNMP-MIB::laLoadInt       Int table
+                "6",  # UCD-SNMP-MIB::laLoadFloat     Float table
+            ],
+        ),
+        SNMPTree(
+            base=".1.3.6.1.2.1.25.3.3.1",  # HOST-RESOURCES-MIB::hrProcessorLoad   One entry per CPU/core
+            oids=[OIDEnd(), "2"],
+        ),
+    ],
     detect=UCD,
 )
